654871c218eb690a18a1f59cfb5a6d054be2edc2
iceage31580 <arnoldomarinero@gmail.com>
2022-12-28 05:01:20 +0000

    Saved progress on Instantiation 2022-12-28 05:01:20 +0000

--- Instantiation/src/main/java/InstantiationExample.java
+++ Instantiation/src/main/java/InstantiationExample.java
@@ -18,6 +18,7 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Type reference = new Type(); 
+        return reference;
     }
 }
