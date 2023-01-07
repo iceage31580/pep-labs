b388493c8f39fab540439f353e893da78c9c638c
iceage31580 <arnoldomarinero@gmail.com>
2023-01-07 10:46:25 +0000

    Saved progress on ThrowCustomException 2023-01-07 10:46:25 +0000

--- ThrowCustomException/src/main/java/CustomException.java
+++ ThrowCustomException/src/main/java/CustomException.java
@@ -1,8 +1,21 @@
+import org.junit.Assert;
+
+import kotlin.jvm.Throws;
 
 /**
  * This class represents a custom exception which may be thrown in the CustomExceptionThrower class.
  * By extending Exception, CustomException has inherited all the behavior of Exception while being its own class!
  * Potentially, this class could also override some the behavior of Exception.
- */
+ */     
 public class CustomException extends Exception{
+    
+    try 
+    {
+        throw new CustomException(); 
+    } 
+    catch (CustomException e)
+    {
+        
+    }
+    
 }
