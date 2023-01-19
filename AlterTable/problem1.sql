04eca7a0e1556db66c1a06edfab67bf2f36a1bf4
iceage31580 <arnoldomarinero@gmail.com>
2023-01-19 23:19:27 +0000

    Saved progress on AlterTable 2023-01-19 23:19:27 +0000

---
+++ AlterTable/problem1.sql
@@ -0,0 +1,5 @@
+ALTER TABLE site_user ADD lastname varchar(100); 
+
+--In problem1.sql, write a SQL statement that adds a "lastname" column to the site_user table, of type
+--     * varchar(100).
+--- ALTER TABLE table_name ADD column_name data_type [constraint];
