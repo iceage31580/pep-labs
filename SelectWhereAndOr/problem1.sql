4e61ab73e3592dddd7a4f7e5a55ee51b683f9483
iceage31580 <arnoldomarinero@gmail.com>
2023-01-19 06:38:50 +0000

    Saved progress on SelectWhereAndOr 2023-01-19 06:38:50 +0000

---
+++ SelectWhereAndOr/problem1.sql
@@ -0,0 +1,7 @@
+SELECT * FROM employee WHERE first_name = 'Steve' AND salary > 75000;
+
+
+--Problem 1: Write a statement that will query the above table for all employees named 'Steve' who earn
+--more than $75,000
+--table: employee
+--
