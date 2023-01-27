a3f3fa84cf56e2a30a82becdf696d8392f7e7120
iceage31580 <arnoldomarinero@gmail.com>
2023-01-27 22:35:04 +0000

    Saved progress on CombineArrays 2023-01-27 22:35:04 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -9,6 +9,9 @@ public class CombineArrays {
      */
     public int[] combine(int[] arr1, int[] arr2){
 
-        return null;
+      int[] result = new int [arr1.length + arr2.length]; 
+      System.arraycopy(arr1, 0, result, 0, arr1.length); 
+      System.arraycopy(arr2, 0,result, arr1.length, arr2.length); 
+      return result; 
     }
 }
